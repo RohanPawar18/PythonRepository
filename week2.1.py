035348db72a150ad77d0d2a327b0e7be6a811b7d
Rohnation <rohanpawar19101996@gmail.com>
2017-08-04 07:51:28 +0530

    Data types and List

---
+++ week2.1.py
@@ -0,0 +1,18 @@
+i=6
+i=6+1.0
+print(type(i))
+s = 'hello\'s hello'
+s=10
+print(s)
+print(type(s))
+
+s="hello"
+print(s[:5])  # Sub-string
+print(s[-3:-1])
+
+#List
+l1 = [True,10,"uhkgxd"]
+print(l1)
+
+l2 = [[2,[37]],4,["abc"]]
+print(l2)
